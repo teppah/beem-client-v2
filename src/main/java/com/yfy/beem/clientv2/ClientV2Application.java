87e1289ed1fdfd4cceaf309a0e7d185463d3c065
Unknown <yifengyang01@gmail.com>
2018-12-17 13:14:21 -0500

    initial IntelliJ commit

---
+++ src/main/java/com/yfy/beem/clientv2/ClientV2Application.java
@@ -0,0 +1,14 @@
+package com.yfy.beem.clientv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientV2Application.class, args);
+    }
+
+}
+
